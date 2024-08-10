b043484f7ac32a22d407237a304e8a5d16c9bbb4
arindam69x <147473276+arindam69x@users.noreply.github.com>
2024-08-10 09:45:02 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -3,6 +3,7 @@ from AnonXMusic.core.dir import dirr
 from AnonXMusic.core.git import git
 from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
+from .ytb import YTB
 
 from .logging import LOGGER
 
